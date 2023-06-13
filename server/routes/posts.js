d2681d320e717d6900ccf6e41a14df68d32217db
rithikkumar3 <111172426+rithikkumar3@users.noreply.github.com>
2023-06-12 22:24:29 -0500

    added dynamoDB codes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,17 +1,24 @@
-// server/routes/posts.js
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/Post');
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const { marshall } = require("@aws-sdk/util-dynamodb");
+
+const ddbClient = new DynamoDBClient({ region: "us-east-2" }); // Replace with your region
 
 router.post('/', async (req, res) => {
-  const post = new Post({
+  const post = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email
-  });
-  
+  };
+
   try {
-    const savedPost = await post.save();
+    const params = {
+      TableName: "finding-joy-table", // replace with your DynamoDB table name
+      Item: marshall(post),
+    };
+    const command = new PutItemCommand(params);
+    const savedPost = await ddbClient.send(command);
     res.json(savedPost);
   } catch (err) {
     res.json({ message: err });
--- server/server.js
+++ server/server.js
@@ -3,22 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const postRoute = require('./routes/posts');
-const DB_URI    = "mongodb+srv://joywithmonica:Lolaluke8@users.nhuvcwz.mongodb.net/?retryWrites=true&w=majority";
 const cors = require('cors');
 app.use(cors());
 
-
 app.use(bodyParser.json());
-
-mongoose.connect(process.env.DB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(() => console.log('Connected to DB!')) 
-    .catch(err => console.log(err));
 
 app.use('/posts', postRoute);
 
 app.listen(3000, () => console.log('Server running'));
-
-
-// mongodb+srv://joywithmonica:Lolaluke8@users.nhuvcwz.mongodb.net/?retryWrites=true&w=majority
