75ebcc1ec3e842a2f06e16f8d2d46e68db82fcb7
rithikkumar3 <111172426+rithikkumar3@users.noreply.github.com>
2023-07-18 00:32:08 -0600

    server.js update

--- server/server.js
+++ server/server.js
@@ -1,3 +1,19 @@
+// require('dotenv').config();
+
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
+// const postRoute = require('./routes/posts');
+// const cors = require('cors');
+// app.use(cors());
+
+// app.use(bodyParser.json());
+
+// app.use('/posts', postRoute);
+
+// app.listen(3000, () => console.log('Server running'));
+
+
 require('dotenv').config();
 
 const express = require('express');
@@ -5,10 +21,21 @@ const app = express();
 const bodyParser = require('body-parser');
 const postRoute = require('./routes/posts');
 const cors = require('cors');
+const path = require('path'); // Add this
+
 app.use(cors());
 
 app.use(bodyParser.json());
 
 app.use('/posts', postRoute);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(3000, () => console.log('Server running'));
