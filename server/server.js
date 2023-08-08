c6939b9f951ac00d35cfb2d9d1d4444e23d6ca70
rithikkumar3 <111172426+rithikkumar3@users.noreply.github.com>
2023-08-08 14:09:46 -0600

    updates

--- server/server.js
+++ server/server.js
@@ -48,21 +48,20 @@ app.post('/save-optin-user', async (req, res) => {
       from: process.env.YOUR_EMAIL, 
       to: req.body.email, 
       subject: 'Thank You for Joining Will I Ever Be Happy? Finding Joy Amidst Depression and Anxiety', 
-      text: `Hello ${req.body.firstName} ${req.body.lastName}, I hope this email finds you well and filled with anticipation for the upcoming Happiness Summit! I wanted to take a moment to express my sincere gratitude for signing up and becoming a part of this incredible journey towards finding true happiness.
-
-      First and foremost, thank you for your trust in our online summit. We understand that your time is valuable, and we are truly honored that you have chosen to invest it in our event.
-      
-      I want to emphasize how thrilled I am to have such an esteemed group of 22 experts joining us to share their wisdom and insights on happiness. Their diverse backgrounds and expertise will provide a rich tapestry of knowledge, offering you a comprehensive understanding of the many facets of happiness. Together, we will explore strategies, tools, and perspectives that can empower you to cultivate lasting happiness in your life.
-      
-      The summit will be a transformative experience, offering you the opportunity to gain invaluable insights, practical advice, and actionable steps. Our experts will guide you through a range of topics, including mindfulness, gratitude, relationships, personal growth, and much more. With their guidance, you will be equipped with the tools to unlock your own happiness potential and create a fulfilling life.
-      Please mark your calendars with the summit dates:  August 21st - 30th
-      Remember, if you know someone that can benefit from this summit, make sure to send them this link  <a href="https://www.findingjoysummit.net">Finding Joy Summit</a> 
-      In the coming days, you will receive further communication from us regarding the event schedule, access details, and any additional resources that will assist you on this happiness journey.
-      If you would like to purchase the package without date and time restrictions,as well as receive a free ticket to my 3-week workshop, “Grooving into Happiness”, please click the link. 
-      <a href="https://www.findingjoysummit.net/thankyou">click here</a>
-      
-      Wishing you joy and happiness`  // Continue with your email content here
-    };
+      html: `
+    <p>Hello ${req.body.firstName} ${req.body.lastName},</p>
+    <p>I hope this email finds you well and filled with anticipation for the upcoming Happiness Summit! I wanted to take a moment to express my sincere gratitude for signing up and becoming a part of this incredible journey towards finding true happiness.</p>
+    <p>First and foremost, thank you for your trust in our online summit. We understand that your time is valuable, and we are truly honored that you have chosen to invest it in our event.</p>
+    <p>I want to emphasize how thrilled I am to have such an esteemed group of 22 experts joining us to share their wisdom and insights on happiness. Their diverse backgrounds and expertise will provide a rich tapestry of knowledge, offering you a comprehensive understanding of the many facets of happiness.</p>
+    <p>Together, we will explore strategies, tools, and perspectives that can empower you to cultivate lasting happiness in your life.</p>
+    <p>The summit will be a transformative experience, offering you the opportunity to gain invaluable insights, practical advice, and actionable steps. Our experts will guide you through a range of topics, including mindfulness, gratitude, relationships, personal growth, and much more.</p>
+    <p>With their guidance, you will be equipped with the tools to unlock your own happiness potential and create a fulfilling life. Please mark your calendars with the summit dates: August 21st - 30th</p>
+    <p>Remember, if you know someone that can benefit from this summit, make sure to send them this <a href="https://www.findingjoysummit.net">link</a>.</p>
+    <p>In the coming days, you will receive further communication from us regarding the event schedule, access details, and any additional resources that will assist you on this happiness journey.</p>
+    <p>If you would like to purchase the package without date and time restrictions, as well as receive a free ticket to my 3-week workshop, “Grooving into Happiness”, please <a href="https://www.findingjoysummit.net/thankyou">click here</a>.</p>
+    <p>Wishing you joy and happiness.</p>
+  `
+};
 
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
@@ -117,11 +116,13 @@ app.post('/save-thankyou-user', async (req, res) => {
       from: process.env.YOUR_EMAIL, 
       to: req.body.email, 
       subject: 'Thank You For Getting Your VIP Pass', 
-      text: `Hello ${req.body.fullName}, Thank you for purchasing the Finding Joy V.I.P. package!!! I'm tickled pink that you have made a decision toward transformation!!! You have an exclusive, seat to an amazing lineup! You will be receiving a link to this spectacular collection of expert speakers. This link will contain the entire Summit and will never expire!. You will recieve this link on August 21st.       
-      In addition, You will also be receiving a link for my upcoming 3 week seminar: "Grooving Into Happiness" that is set to launch in October as part of the VIP package!!! 
-      
-      I'm so happy you have decided to take this journey with us!!!!` // Continue with your email content here
-    };
+      html: `
+    <p>Hello ${req.body.fullName},</p>
+    <p>Thank you for purchasing the Finding Joy V.I.P. package!!! I'm tickled pink that you have made a decision toward transformation!!! You have an exclusive seat to an amazing lineup! You will be receiving a link to this spectacular collection of expert speakers. This link will contain the entire Summit and will never expire! You will receive this link on August 21st.</p>
+    <p>In addition, You will also be receiving a link for my upcoming 3-week seminar: "Grooving Into Happiness" that is set to launch in October as part of the VIP package!!!</p>
+    <p>I'm so happy you have decided to take this journey with us!!!!</p>
+  `
+};
 
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
